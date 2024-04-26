7b06f13c0741058991ea8977007f9391e1fa789f
MekaylaMoore <144389177+MekaylaMoore@users.noreply.github.com>
2024-04-26 12:11:54 -0700

    Gateway SKU Migration

---
+++ expressroute-gateway/gateway-migration/migration.ps1
@@ -0,0 +1,82 @@
+﻿# Need to verify PS module to ensure have the new API for PUT Gateway
+
+
+# Start preparing
+$gatewayUriDisabled = Read-Host "Migrate from Gateway Resource ID"
+$gatewayUriEnabled = Read-Host "Migrate to Gateway Resource ID"
+$subIdRegex = "subscriptions/"
+$resourceGroupRegex = "resourceGroups/"
+$vnetRegex = "virtualNetworks/"
+$subId = $gatewayUriDisabled.Substring($gatewayUriDisabled.ToLower().IndexOf($subIdRegex.ToLower()) + $subIdRegex.Length, $gatewayUriDisabled.ToLower().IndexOf($resourceGroupRegex.ToLower()) - $gatewayUriDisabled.ToLower().IndexOf($subIdRegex.ToLower()) - $subIdRegex.Length -1)
+Write-Host "Customer Subscription ID:" $subId
+Connect-AzAccount -WarningAction Ignore | Out-Null
+Select-AzSubscription -Subscription $subId -Force  | Out-null
+$gatewayDisabled = Get-AzResource -ResourceId $gatewayUriDisabled
+$gatewayEnabled =  Get-AzResource -ResourceId $gatewayUriEnabled
+$gwtDisabled = Get-AzVirtualNetworkGateway -Name $gatewayDisabled.Name -ResourceGroupName $gatewayDisabled.ResourceGroupName
+$gwtEnabled = Get-AzVirtualNetworkGateway -Name $gatewayEnabled.Name -ResourceGroupName $gatewayEnabled.ResourceGroupName
+$resourceGroup = $gatewayDisabled.ResourceGroupName
+# Validate all connections and gateways
+if($gwtEnabled.provisioningState -ne "Succeeded")
+{
+          Write-Host $gwtEnabled.Name " is " $gwtEnabled.provisioningState
+          Read-Host "Enter anything to exit, Migration failed"
+          exit
+}
+
+$connections = Get-AzVirtualNetworkGatewayConnection -ResourceGroupName $resourceGroup | Where-Object -FilterScript {$_.VirtualNetworkGateway1.Id -eq $gatewayUriEnabled}
+foreach($connection in $connections)
+{
+        if($connection.ProvisioningState -ne "Succeeded")
+        {
+          Write-Host $connection.Name " is " $connection.ProvisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+        }
+}
+
+if($gwtDisabled.provisioningState -ne "Succeeded")
+{
+          Write-Host $gwtDisabled.Name " is " $gwtDisabled.provisioningState
+          Read-Host "Enter anything to exit, Migration failed"
+          exit
+}
+
+$connections = Get-AzVirtualNetworkGatewayConnection -ResourceGroupName $resourceGroup | Where-Object -FilterScript {$_.VirtualNetworkGateway1.Id -eq $gatewayUriDisabled}
+foreach($connection in $connections)
+{
+        if($connection.ProvisioningState -ne "Succeeded")
+        {
+          Write-Host $connection.Name " is " $connection.ProvisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+        }
+}
+
+# Migrating traffic 
+$startTime = Get-Date
+
+Write-Host "---------------- Enabling gateway" $gwtEnabled.Name "----------------"
+$gwt1 = Set-AzVirtualNetworkGateway -VirtualNetworkGateway $gwtEnabled -AdminState Enabled
+
+if($gwt1.ProvisioningState -ne "Succeeded")
+{
+          Write-Host "Not able to enable" $gwt1.Name
+          Read-Host "Enter anything to exit, Migration failed"
+          exit
+}
+
+Write-Host "---------------- Disabling gateway" $gwtDisabled.Name "----------------"
+$gwt2 = Set-AzVirtualNetworkGateway -VirtualNetworkGateway $gwtDisabled -AdminState Disabled
+
+if($gwt2.ProvisioningState -ne "Succeeded")
+{
+          Write-Host "Not able to disable" $gwt2.Name
+          Read-Host "Enter anything to exit, Migration failed"
+          exit
+}
+$endTime = Get-Date
+$diff =  New-TimeSpan -Start $startTime -End $endTime
+# Migration completed!
+Write-Host "---------------- Migration from" $gatewayUriDisabled "to" $gatewayUriEnabled "is completed! Taking" $diff.TotalMinutes "minutes----------------"
+Read-Host "Enter anything to exit"
---
+++ expressroute-gateway/gateway-migration/preparemigration.ps1
@@ -0,0 +1,126 @@
+﻿# Need to verify PS module to ensure have the new API for PUT Gateway
+
+
+# Start preparing
+$gatewayUri = Read-Host "Prepare Migration: Please Enter Gateway Resource ID"
+$subIdRegex = "subscriptions/"
+$resourceGroupRegex = "resourceGroups/"
+$vnetRegex = "virtualNetworks/"
+$subId = $gatewayUri.Substring($gatewayUri.ToLower().IndexOf($subIdRegex.ToLower()) + $subIdRegex.Length, $gatewayUri.ToLower().IndexOf($resourceGroupRegex.ToLower()) - $gatewayUri.ToLower().IndexOf($subIdRegex.ToLower()) - $subIdRegex.Length -1)
+Write-Host "Customer Subscription ID:" $subId
+Connect-AzAccount -WarningAction Ignore | Out-Null
+Select-AzSubscription -Subscription $subId -Force -WarningAction Ignore | Out-null
+
+Write-Host "Getting existing resources for gateway:" $gatewayUri
+$gateway =  Get-AzResource -ResourceId $gatewayUri  -WarningAction Ignore
+$resourceGroup = $gateway.ResourceGroupName
+$location = $gateway.Location
+$pip = Get-AzResource -ResourceId $gateway.Properties.ipConfigurations[0].properties.publicIPAddress.id
+$subnet =  Get-AzResource -ResourceId $gateway.Properties.ipConfigurations[0].properties.subnet.id
+$vnetName = $subnet.ParentResource.Substring($subnet.ParentResource.ToLower().IndexOf($vnetRegex.ToLower()) + $vnetRegex.Length, $subnet.ParentResource.Length - $subnet.ParentResource.ToLower().IndexOf($vnetRegex.ToLower()) - $vnetRegex.Length)
+$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $vnet
+
+# Verify there are at least 2 prefixes in gateway subnet, if not, ask customer add one
+if($subnet.AddressPrefix.Count -lt 2)
+{
+      Write-Host  "Gateway Subnet has " $subnet.AddressPrefix.Count "prefixes, needs at least 2, please add one more prefix"
+      $prefix = Read-Host "Enter new prefix"
+      $subnet.AddressPrefix.Add($prefix)
+      Set-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $vnet -AddressPrefix $subnet.AddressPrefix | Out-null
+      Set-AzVirtualNetwork -VirtualNetwork $vnet | Out-null
+      $vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup
+      $subnet = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $vnet
+}
+
+if($subnet.AddressPrefix.Count -lt 2)
+{
+      Write-Host  "Gateway Subnet has " $subnet.AddressPrefix.Count "prefixes, needs at least 2, please add one more prefix"
+      Read-Host "Enter anything to exit, Prepare for migration failed"
+      exit
+}
+
+# Verify all resources are in succeeded state
+if($gateway.Properties.provisioningState -ne "Succeeded")
+{
+          Write-Host $gateway.Name " is " $gateway.Properties.provisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+}
+
+$connections = Get-AzVirtualNetworkGatewayConnection -ResourceGroupName $resourceGroup | Where-Object -FilterScript {$_.VirtualNetworkGateway1.Id -eq $gatewayUri}
+foreach($connection in $connections)
+{
+        if($connection.ProvisioningState -ne "Succeeded")
+        {
+          Write-Host $connection.Name " is " $connection.ProvisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+        }
+}
+
+Write-Host "---------------- All validation passed, start creating new resources ----------------"
+
+# Getting input from customer and create new resources
+
+$prefix = Read-Host "Please choose the suffix for new resources, new resource name will be existingresourcename_<suffix>"
+$pipName = $pip.Name + "_" + $prefix
+$ipconfigName = $gateway.Properties.ipConfigurations[0].name + "_" + $prefix
+$gatewayName = $gateway.Name + "_" + $prefix
+$zone = Read-Host "Please select zones for new gateway"
+$gatewaySku = Read-Host "Please choose the sku for new gateway [ErGw1AZ|ErGw2AZ|ErGw3AZ]"
+
+if($pipName.Length -gt 80)
+{
+    $pipName = $pipName.Substring(0,80)
+}
+
+if($ipconfigName.Length -gt 80)
+{
+    $ipconfigName = $ipconfigName.Substring(0,80)
+}
+
+if($gatewayName.Length -gt 80)
+{
+    $gatewayName = $gatewayName.Substring(0,80)
+}
+
+$pipNew = New-AzPublicIpAddress -Name $pipName -ResourceGroupName $resourceGroup -Location $location -AllocationMethod Static -Sku Standard -Zone $zone -Force
+$subnetNew = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $vnet
+$ipconfNew = New-AzVirtualNetworkGatewayIpConfig -Name $ipconfigName -Subnet $subnetNew -PublicIpAddress $pipNew
+$startTime = Get-Date
+Write-Host "---------------- Creating new gateway" $gatewayName "Sku" $gatewaySku "----------------" 
+New-AzVirtualNetworkGateway -Name $gatewayName -ResourceGroupName $resourceGroup -Location $location -IpConfigurations $ipconfNew -GatewayType Expressroute -GatewaySku $gatewaysku -AdminState Disabled -Force | Out-null
+$gatewayNew = get-AzVirtualNetworkGateway -Name $gatewayName -ResourceGroupName $resourceGroup
+if($gatewayNew.ProvisioningState -ne "Succeeded")
+{
+          Write-Host $gatewayNew.Name " is " $gateway.ProvisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+}
+
+
+foreach($connection in $connections)
+{
+        $connName = $connection.Name + "_" + $prefix
+        $circuitId = $connection.Peer.Id
+        Write-Host "---------------- Creating new connection" $connName "with circuit" $circuitId "----------------"
+        New-AzVirtualNetworkGatewayConnection -Name $connName -ResourceGroupName $resourceGroup -Location $location -VirtualNetworkGateway1 $gatewayNew -PeerId $circuitId -ConnectionType ExpressRoute | Out-null
+}
+
+$connectionsNew = Get-AzVirtualNetworkGatewayConnection -ResourceGroupName $resourceGroup | Where-Object -FilterScript {$_.VirtualNetworkGateway1.Name -contains $prefix} 
+foreach($connection in $connectionsNew)
+{
+        if($connection.ProvisioningState -ne "Succeeded")
+        {
+          Write-Host $connection.Name " is " $connection.ProvisioningState
+          Read-Host "Enter anything to exit, Prepare for migration failed"
+          exit
+        }
+}
+
+$endTime = Get-Date
+$diff =  New-TimeSpan -Start $startTime -End $endTime
+# Preparetion completed!
+Write-Host "---------------- Prepare for migration for" $gatewayUri "is completed! Taking" $diff.TotalMinutes "minutes ----------------" 
+Read-Host "Enter anything to exit"
