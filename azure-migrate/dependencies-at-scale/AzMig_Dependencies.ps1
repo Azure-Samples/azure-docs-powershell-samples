e3973d777b5153afc7829de7f31c452531faeadd
Yashita LNU <t-yashitalnu@microsoft.com>
2024-06-20 21:34:21 +0530

    change in query

--- azure-migrate/dependencies-at-scale/AzMig_Dependencies.ps1
+++ azure-migrate/dependencies-at-scale/AzMig_Dependencies.ps1
@@ -146,7 +146,22 @@ function Get-AzMigDiscoveredVMwareVMs {
         $query1 = "
 migrateresources
 | where id has '$SiteId' and type in ('microsoft.offazure/serversites/machines', 'microsoft.offazure/hypervsites/machines', 'microsoft.offazure/vmwaresites/machines')
+| mv-expand properties.networkAdapters
+| extend IPAddressList = properties_networkAdapters.ipAddressList
+| summarize IPAddresses = make_list(IPAddressList) by name
+| join kind=inner (
+    migrateresources
+    | where id has '$SiteId' and type in ('microsoft.offazure/serversites/machines', 'microsoft.offazure/hypervsites/machines', 'microsoft.offazure/vmwaresites/machines')
+) on name
+| join kind=leftouter (
+    migrateresources
+    | where id has '$SiteId' and type in ('microsoft.offazure/serversites/machines', 'microsoft.offazure/hypervsites/machines', 'microsoft.offazure/vmwaresites/machines')
+    | mv-expand properties.dependencyMapDiscovery.errors
+    | extend ErrorDetails = strcat('ID:', properties_dependencyMapDiscovery_errors.id, ', Code:', properties_dependencyMapDiscovery_errors.code, ', Message:', properties_dependencyMapDiscovery_errors.message)
+    | summarize Error = make_list(ErrorDetails) by name
+) on name
 | extend ServerName = properties.displayName,
+         DependencyErrors = strcat('DependencyScopeStatus:', properties.dependencyMapDiscovery.discoveryScopeStatus, ' Errors:', Error),
          DependencyStatus = iff(array_length(properties.dependencyMapDiscovery.errors) == 0, properties.dependencyMapping, 'ValidationFailed'),
          Source = properties.vCenterFQDN,
          ErrorTimeStamp = properties.updatedTimestamp,
@@ -157,16 +172,6 @@ migrateresources
          FriendlyNameOfCredentials = properties.dependencyMapDiscovery.hydratedRunAsAccountId,
          Tags = tags,
          ARMID = id
-| mv-expand properties.networkAdapters
-| extend IPAddressList = properties_networkAdapters.ipAddressList
-| summarize IPAddresses = make_list(IPAddressList) by name,tostring(ServerName),tostring(DependencyStatus),tostring(Source),tostring(ErrorTimeStamp),tostring(DependencyStartTime),tostring(OperatingSystem),tostring(PowerStatus),tostring(Appliance),tostring(FriendlyNameOfCredentials),tostring(Tags),tostring(ARMID),tostring(properties.dependencyMapDiscovery)
-|join kind=leftouter (
-    migrateresources
-    | mv-expand properties.dependencyMapDiscovery.errors
-    | extend ErrorDetails = strcat('ID:', properties_dependencyMapDiscovery_errors.id, ', Code:', properties_dependencyMapDiscovery_errors.code, ', Message:', properties_dependencyMapDiscovery_errors.message)
-    | summarize Error = make_list(ErrorDetails) by name
-) on name
-|extend DependencyErrors = strcat('DependencyScopeStatus:', todynamic(properties_dependencyMapDiscovery).discoveryScopeStatus, ' Errors:', Error)
 | project ServerName, Source, DependencyStatus, DependencyErrors, ErrorTimeStamp, DependencyStartTime, OperatingSystem, PowerStatus, Appliance, FriendlyNameOfCredentials, Tags, ARMID
 "
 
