243433b67047eae362d3d3dafe5ddb0b7880a449
Yashita LNU <t-yashitalnu@microsoft.com>
2024-06-25 15:48:32 +0530

    ...

--- azure-migrate/dependencies-at-scale/AzMig_Dependencies.ps1
+++ azure-migrate/dependencies-at-scale/AzMig_Dependencies.ps1
@@ -359,7 +359,7 @@ function Set-AzMigDependencyMappingAgentless {
     }
     $Properties = GetRequestProperties
 
-    $VMs = ($VMDetails | Select-Object -ExpandProperty "ARMID")
+    $VMs = ($VMDetails | Select-Object -ExpandProperty "ARM  ID")
 
 
     $VMs = $VMs | sort
